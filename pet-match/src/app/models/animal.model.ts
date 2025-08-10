2ea15ea4258c23016eaba0da6760fe5ee20e14cd
viki-tanchevva <tancheva.viktoria@gmail.com>
2025-08-10 20:10:56 +0300

    Added the models

---
+++ pet-match/src/app/models/animal.model.ts
@@ -0,0 +1,15 @@
+type AnimalType = 'dog' | 'cat' | 'rabbit' | 'guinea pig' | 'hamster' | 'bird' | 'turtle' | 'other'
+                | 'Dog' | 'Cat' | 'Rabbit' | 'Guinea pig' | 'Hamster' | 'Bird' | 'Turtle' | 'Other';
+
+export interface Animal {
+  id: string;
+  name: string;
+  type: AnimalType;
+  age: number;
+  location: string;
+  imageUrl: string;
+  description: string;
+  addedByUserId?: string | null;
+  likes?: number;
+  adopted?: boolean;
+}
