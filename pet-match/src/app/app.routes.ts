321a179b4e8082d48f250769627d4acd742491c7
viki-tanchevva <tancheva.viktoria@gmail.com>
2025-08-11 01:57:35 +0300

    Commit

--- pet-match/src/app/app.routes.ts
+++ pet-match/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home.component').then(c => c.HomeComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login.component').then(c => c.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register.component').then(c => c.RegisterComponent)
+    },
+    {
+        path: 'add-animal',
+        loadComponent: () => import('./features/animals/create-animal/create-animal.component').then(c => c.CreateAnimalComponent)
+    },
+    {
+        path: 'animals',
+        loadComponent: () => import('./features/animals/animals-board/animals-board.component').then(c => c.AnimalsBoardComponent)
+    },
+    {
+        path: 'animals/:id',
+        loadComponent: () => import('./features/animals/animal-details/animal-details.component').then(c => c.AnimalDetailsComponent)
+    }
+];
